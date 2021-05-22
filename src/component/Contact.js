7f65e6a8c1a0586836aeca973762644987ce71f8
DongMZ1 <915094594@qq.com>
2021-05-22 14:29:49 -0400

    update href for icons

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../SCSS/main.scss'
-import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineLinkedin, AiOutlineFacebook, AiOutlineInstagram} from 'react-icons/ai'
 const Contact = () => {
     return(
        <div className='contact' id='spcontact'>
-           <a><AiOutlineLinkedin className='icon' /></a>
+           <a href='https://www.linkedin.com/in/mingzhoudong/'><AiOutlineLinkedin className='icon' /></a>
+           <a href='https://www.facebook.com/mingming.dong.79'><AiOutlineFacebook className='icon' /></a>
+           <a href='https://www.instagram.com/partinggggton/'><AiOutlineInstagram className='icon' /></a>
+
        </div>      
     )
 }
