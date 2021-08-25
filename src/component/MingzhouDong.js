a0b6ce3bc0f05e58bf9d7ccab657d9580bbd8306
DongMZ1 <915094594@qq.com>
2021-08-25 15:21:03 -0400

    add mern pictures

--- src/component/MingzhouDong.js
+++ src/component/MingzhouDong.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import '../SCSS/main.scss'
+import React from 'react';
+import '../SCSS/main.scss';
 import Typist from 'react-typist';
-import image from '../tx.jpg'
+import image from '../tx.jpg';
+import m from '../image/m.png';
+import e from '../image/e.png';
+import r from '../image/r.png';
+import n from '../image/n.png';
 
-const MingzhouDong = ({setisProjectPage}) => {
+const MingzhouDong = ({ setisProjectPage }) => {
     return (
-        <div className='row mingzhoudong m-5' id='spmingzhoudong'>
-            <div className='col-sm'>
+        <div className={`row mingzhoudong ${window.innerWidth > 768 && 'm-5'}`} id='spmingzhoudong'>
+            <div className='col-sm display-none'>
                 <Typist avgTypingDelay={0.1}>
                     <h1>Hi!</h1>
-                    <p>
                         I am <b> Mingzhou Dong </b>
                         Creative experienced web developer with a passion for building tools ,working with the latest technologies, and never give up learning.
-                    </p>
                 </Typist>
-                <div className='disp-flex'>
-                    <div onClick={()=>setisProjectPage(true)} className='project-icon mx-auto'>See My Projects</div>
+                <div className='row my-3'><img className='mern' src={m}></img><img className='mern' src={e}></img><img className='mern' src={r}></img><img className='mern' src={n}></img></div>
+                <div className='disp-flex row mb-3'>
+                    <div onClick={() => setisProjectPage(true)} className='project-icon mx-auto'>See My Projects</div>
                 </div>
             </div>
             <div className='col-sm'>
